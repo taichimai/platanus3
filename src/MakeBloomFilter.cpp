d66cab1b10e7e880688474d8cc221cfeb85c95f3
taichimai <imai.t.an@m.titech.ac.jp>
2023-01-23 20:31:41 +0900

    v0.5

--- src/MakeBloomFilter.cpp
+++ src/MakeBloomFilter.cpp
@@ -6,21 +6,14 @@
 #include"bloomfilter.cpp"
 
 template<typename BITLENGTH>
-BF<BITLENGTH> MakeBF(ReadSet *RS,uint64_t filtersize ,uint8_t numhashes,int kmer_length);
-
-template<typename BITLENGTH>
 BF<BITLENGTH> MakeBF(ReadSet *RS,uint64_t filtersize ,uint8_t numhashes,int kmer_length){
     BF<BITLENGTH> Kmer_BF(filtersize,numhashes);
 
-    BITLENGTH mp_Fw[100];
-    BITLENGTH A_Fw(0),C_Fw(1),G_Fw(2),T_Fw(3);
-    mp_Fw['A']=A_Fw; mp_Fw['C']=C_Fw; mp_Fw['G']=G_Fw; mp_Fw['T']=T_Fw;
-    BITLENGTH mp_Bw[100];
-    BITLENGTH A_Bw(3); A_Bw<<=(kmer_length*2-2);
-    BITLENGTH C_Bw(2); C_Bw<<=(kmer_length*2-2);
-    BITLENGTH G_Bw(1); G_Bw<<=(kmer_length*2-2);
-    BITLENGTH T_Bw(0); T_Bw<<=(kmer_length*2-2);
-    mp_Bw['A']=A_Bw; mp_Bw['C']=C_Bw; mp_Bw['G']=G_Bw; mp_Bw['T']=T_Bw;
+    BITLENGTH A_right(0); BITLENGTH A_left=(A_right<<(kmer_length*2-2)); 
+    BITLENGTH C_right(1); BITLENGTH C_left=(C_right<<(kmer_length*2-2));
+    BITLENGTH G_right(2); BITLENGTH G_left=(G_right<<(kmer_length*2-2));
+    BITLENGTH T_right(3); BITLENGTH T_left=(T_right<<(kmer_length*2-2));
+    std::vector<BITLENGTH> end_bases={A_left,C_left,G_left,T_left,A_right,C_right,G_right,T_right};
     
     #pragma omp parallel for  num_threads(4) 
     for(size_t b=0;b<(*RS).bucket_count();b++)
@@ -31,9 +24,8 @@ BF<BITLENGTH> MakeBF(ReadSet *RS,uint64_t filtersize ,uint8_t numhashes,int kmer
         BITLENGTH KmerItem=CompareBit(kmer_Fw,kmer_Bw,kmer_length*2);       
         Kmer_BF.add(&KmerItem,kmer_length*2);
         for (int i=kmer_length;i<target_read.size();i++){
-            kmer_Fw=((kmer_Fw<<2)| mp_Fw[target_read[i]]);
-            kmer_Bw=((kmer_Bw>>2)| mp_Bw[target_read[i]]);
-
+            kmer_Fw=((kmer_Fw<<2)| end_bases[ base_to_bit[ target_read[i] ] + 4 ] );
+            kmer_Bw=((kmer_Bw>>2)| end_bases[ base_to_bit[ trans_base[target_read[i]] ] ] );
             KmerItem=CompareBit(kmer_Fw,kmer_Bw,kmer_length*2);
             Kmer_BF.add(&KmerItem,kmer_length*2);
         }
