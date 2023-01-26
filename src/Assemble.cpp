99c4e4d34f07fdfd7490f02cfa6e1d4b923d921d
taichimai <imai.t.an@m.titech.ac.jp>
2023-01-26 13:01:16 +0900

    v0.61

--- src/Assemble.cpp
+++ src/Assemble.cpp
@@ -6,8 +6,10 @@ template<typename BITLENGTH>
 int Assemble (ReadFile &input_reads,Options &parameters){
     KmerSet seed_kmer=input_reads.GetSeedKmer(parameters.kmer_length);
     BF<BITLENGTH> first_bloom_filter=MakeBF<BITLENGTH>(input_reads.reads,parameters.filter_size,parameters.num_hashes,parameters.kmer_length); //make bloomfilter
+    std::cerr<<"bloom filter loaded"<<std::endl;
     DeBruijnGraph<BITLENGTH > first_dbg(parameters.kmer_length,first_bloom_filter);   //make debruijngraph
     first_dbg.MakeDBG(seed_kmer,parameters.filter_size,parameters.num_hashes);
+    std::cerr<<"de bruijn graph loaded"<<std::endl;
     first_dbg.CountNodeCoverage(input_reads.reads);
     first_dbg.PrintGraph();
 
